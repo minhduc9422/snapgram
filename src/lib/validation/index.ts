3e380ebb447471d75d8f074eeedff5500be9c97d
Phạm Minh Đức <minhduc9422@gmail.com>
2024-03-18 13:55:32 +0700

    fist commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,21 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." }),
+  confirmPassword: z
+    .string()
+    .refine(
+      ({ confirmPassword }, { password }) => confirmPassword === password,
+      {
+        message: "Passwords do not match.",
+        path: ["confirmPassword"],
+      }
+    ),
+});
