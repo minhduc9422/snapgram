e8d46179d7353f9379b92560c0f67af81c4a8f4b
Phạm Minh Đức <minhduc9422@gmail.com>
2024-03-20 23:06:35 +0700

    update home, and create post

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ import RootLayout from "./_root/RootLayout";
 import Home from "./_root/pages/Home";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 
 const App = () => {
   return (
@@ -21,6 +31,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -57,7 +57,7 @@ const SigninForm = () => {
 
   return (
     <Form {...form}>
-      <div className="flex flex-center flex-col">
+      <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
